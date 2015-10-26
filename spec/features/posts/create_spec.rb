5fad062d9ef643c97ed4569d152dfdf4ce1a9bcb
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-10-26 17:09:53 +0200

    Add test for body presence validation(tests red)

--- spec/features/posts/create_spec.rb
+++ spec/features/posts/create_spec.rb
@@ -22,4 +22,16 @@ describe 'Creating posts' do
 
     expect(page).to have_content('error')
   end
+
+  it 'displays an error when the body has no title' do
+    visit '/'
+    click_link 'New Post'
+    expect(page).to have_content('New Post')
+
+    fill_in 'Title', with: 'First post'
+    fill_in 'Body', with: ''
+    click_button 'Create Post'
+
+    expect(page).to have_content('error')
+  end
 end
