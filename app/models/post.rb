7eb3aa51b7f5c4aa6b38ccf6bf74a74c566453a9
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-10-26 17:28:52 +0200

    Refactor code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: { in: 5..100 }, uniqueness: true
+  validates :title, length: { in: 5..100 }, uniqueness: true
   validates :body, presence: true
 end
